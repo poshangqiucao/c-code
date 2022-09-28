97cda1be2756c6df51ea4d6ca687070e482fcd94
poshang <2890122341@qq.com>
2022-09-28 10:53:14 +0800

    code

--- base/hello.c
+++ base/hello.c
@@ -15,7 +15,13 @@ int main() {
     printf("max val is %d\n", c);
     int d;
     scanf("%d", &d);
-    printf("val d is %d", d);
+    printf("val d is %d\n", d);
+    printf("a size is : %d\n", sizeof a);
+    printf("int size is %d\n", sizeof(int));
+    printf("double size is %d\n", sizeof(double));
+    printf("float size is %d\n", sizeof(float));
+    printf("char size is %d\n", sizeof(char));
+    // printf("bool size is %d", sizeof(bool));
     return 0;
 }
 
